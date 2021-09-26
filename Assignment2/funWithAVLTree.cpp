29df1b2cf514c48c8bc47501f85f48fa7674a313
MalevolentStrix <jithintvm1@gmail.com>
2021-09-26 14:20:00 +0530

    Beautify code

--- Assignment2/funWithAVLTree.cpp
+++ Assignment2/funWithAVLTree.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
@@ -12,18 +11,106 @@ public:
     int height;
 };
 
-int max(int a, int b);
+int sumvariable = 0, numbervariable = 0;
+int maximumAmongTwo(int a, int b);
+int checkDifferenceBetweenNodeHeights(Node *N);
+int sumOfKeys(Node *root);
+int sumOfKeysCaller(Node *root);
+int totalNumberOfNodes(Node *root);
+int totalNumberOfNodesCaller(Node *root);
+int heightOfTheNode(Node *N);
+Node *rightRotate(Node *y);
+Node *leftRotate(Node *x);
+Node *insertNewNode(Node *node, int key);
+Node *minValueNode(Node *node);
+Node *maxValueNode(Node *node);
+Node *deleteNode(Node *root, int key);
+Node *searchNodeForKey(Node *root, int key);
 
-int height(Node *N)
+int main()
 {
-    if (N == NULL)
-        return 0;
-    return N->height;
+    Node *root = NULL;
+    string numberOfLines, eachLine;
+    int integerInsert, integerDelete, lastLineInteger;
+    getline(cin, numberOfLines);
+    int numberOfLinesInteger = stoi(numberOfLines);
+    for (int i = 0; i < numberOfLinesInteger; i++)
+    {
+        getline(cin, eachLine);
+        stringstream ss(eachLine);
+        string wordSplitter;
+        string eachLineSplitIntoWords[1000];
+        for (int j = 0; ss >> wordSplitter; j++)
+        {
+            eachLineSplitIntoWords[j] = wordSplitter;
+        }
+
+        if ((eachLineSplitIntoWords[0].compare("A")) == 0)
+        {
+            for (int iterator = 1; eachLineSplitIntoWords[iterator].compare("\0") != 0; iterator++)
+            {
+
+                integerInsert = stoi(eachLineSplitIntoWords[iterator]);
+                root = insertNewNode(root, integerInsert);
+            }
+        }
+        else if ((eachLineSplitIntoWords[0].compare("R")) == 0)
+        {
+            for (int iterator = 1; eachLineSplitIntoWords[iterator].compare("\0") != 0; iterator++)
+            {
+
+                integerDelete = stoi(eachLineSplitIntoWords[iterator]);
+                root = deleteNode(root, integerDelete);
+            }
+        }
+        else
+        {
+            for (int iterator = 0; eachLineSplitIntoWords[iterator].compare("\0") != 0; iterator++)
+            {
+                lastLineInteger = stoi(eachLineSplitIntoWords[iterator]);
+                Node *thisisit = searchNodeForKey(root, lastLineInteger);
+                if (thisisit != NULL)
+                {
+                    int heightofkey = heightOfTheNode(thisisit);
+                    cout << heightofkey;
+                    cout << " " << minValueNode(thisisit)->key;
+                    cout << " " << maxValueNode(thisisit)->key;
+                    int sum = sumOfKeysCaller(thisisit);
+                    int totalNumberOfNodes = totalNumberOfNodesCaller(thisisit);
+                    double average = (sum * 1.0) / totalNumberOfNodes;
+                    float value = (int)(average * 10 + .5);
+                    cout << " " << (float)value / 10 << endl;
+                }
+                else
+                {
+                    cout << "-1" << endl;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
-int max(int a, int b)
+int heightOfTheNode(Node *N)
 {
-    return (a > b) ? a : b;
+    if (N == NULL)
+    {
+        return 0;
+    }
+    int heightOfTheNode = N->height;
+    return heightOfTheNode;
+}
+
+int maximumAmongTwo(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
 }
 
 Node *newNode(int key)
@@ -33,7 +120,6 @@ Node *newNode(int key)
     node->left = NULL;
     node->right = NULL;
     node->height = 1;
-
     return (node);
 }
 
@@ -45,12 +131,8 @@ Node *rightRotate(Node *y)
     x->right = y;
     y->left = T2;
 
-    y->height = max(height(y->left),
-                    height(y->right)) +
-                1;
-    x->height = max(height(x->left),
-                    height(x->right)) +
-                1;
+    y->height = maximumAmongTwo(heightOfTheNode(y->left), heightOfTheNode(y->right)) + 1;
+    x->height = maximumAmongTwo(heightOfTheNode(x->left), heightOfTheNode(x->right)) + 1;
 
     return x;
 }
@@ -63,47 +145,55 @@ Node *leftRotate(Node *x)
     y->left = x;
     x->right = T2;
 
-    x->height = max(height(x->left),
-                    height(x->right)) +
-                1;
-    y->height = max(height(y->left),
-                    height(y->right)) +
-                1;
+    x->height = maximumAmongTwo(heightOfTheNode(x->left), heightOfTheNode(x->right)) + 1;
+    y->height = maximumAmongTwo(heightOfTheNode(y->left), heightOfTheNode(y->right)) + 1;
 
     return y;
 }
 
-int getBalance(Node *N)
+int checkDifferenceBetweenNodeHeights(Node *N)
 {
     if (N == NULL)
+    {
         return 0;
-    return height(N->left) -
-           height(N->right);
+    }
+    return heightOfTheNode(N->left) - heightOfTheNode(N->right);
 }
 
-Node *insert(Node *node, int key)
+Node *insertNewNode(Node *node, int key)
 {
 
     if (node == NULL)
+    {
         return (newNode(key));
+    }
 
     if (key < node->key)
-        node->left = insert(node->left, key);
+    {
+        node->left = insertNewNode(node->left, key);
+    }
     else if (key > node->key)
-        node->right = insert(node->right, key);
+    {
+        node->right = insertNewNode(node->right, key);
+    }
     else
+    {
         return node;
+    }
 
-    node->height = 1 + max(height(node->left),
-                           height(node->right));
+    node->height = 1 + maximumAmongTwo(heightOfTheNode(node->left), heightOfTheNode(node->right));
 
-    int balance = getBalance(node);
+    int balance = checkDifferenceBetweenNodeHeights(node);
 
     if (balance > 1 && key < node->left->key)
+    {
         return rightRotate(node);
+    }
 
     if (balance < -1 && key > node->right->key)
+    {
         return leftRotate(node);
+    }
 
     if (balance > 1 && key > node->left->key)
     {
@@ -125,8 +215,9 @@ Node *minValueNode(Node *node)
     Node *current = node;
 
     while (current->left != NULL)
+    {
         current = current->left;
-
+    }
     return current;
 }
 
@@ -135,8 +226,9 @@ Node *maxValueNode(Node *node)
     Node *current = node;
 
     while (current->right != NULL)
+    {
         current = current->right;
-
+    }
     return current;
 }
 
@@ -144,14 +236,19 @@ Node *deleteNode(Node *root, int key)
 {
 
     if (root == NULL)
+    {
         return root;
+    }
 
     if (key < root->key)
+    {
         root->left = deleteNode(root->left, key);
+    }
 
     else if (key > root->key)
+    {
         root->right = deleteNode(root->right, key);
-
+    }
     else
     {
 
@@ -174,39 +271,37 @@ Node *deleteNode(Node *root, int key)
         {
 
             Node *temp = minValueNode(root->right);
-
             root->key = temp->key;
-
             root->right = deleteNode(root->right,
                                      temp->key);
         }
     }
 
     if (root == NULL)
+    {
         return root;
+    }
+    root->height = 1 + maximumAmongTwo(heightOfTheNode(root->left), heightOfTheNode(root->right));
 
-    root->height = 1 + max(height(root->left),
-                           height(root->right));
+    int balance = checkDifferenceBetweenNodeHeights(root);
 
-    int balance = getBalance(root);
-
-    if (balance > 1 &&
-        getBalance(root->left) >= 0)
+    if (balance > 1 && checkDifferenceBetweenNodeHeights(root->left) >= 0)
+    {
         return rightRotate(root);
+    }
 
-    if (balance > 1 &&
-        getBalance(root->left) < 0)
+    if (balance > 1 && checkDifferenceBetweenNodeHeights(root->left) < 0)
     {
         root->left = leftRotate(root->left);
         return rightRotate(root);
     }
 
-    if (balance < -1 &&
-        getBalance(root->right) <= 0)
+    if (balance < -1 && checkDifferenceBetweenNodeHeights(root->right) <= 0)
+    {
         return leftRotate(root);
+    }
 
-    if (balance < -1 &&
-        getBalance(root->right) > 0)
+    if (balance < -1 && checkDifferenceBetweenNodeHeights(root->right) > 0)
     {
         root->right = rightRotate(root->right);
         return leftRotate(root);
@@ -215,50 +310,43 @@ Node *deleteNode(Node *root, int key)
     return root;
 }
 
-void preOrder(Node *root)
-{
-    if (root != NULL)
-    {
-        cout << root->key << " ";
-        preOrder(root->left);
-        preOrder(root->right);
-    }
-}
-int sumvariable = 0;
-int sumofnodes(Node *root)
+int sumOfKeys(Node *root)
 {
     if (root != NULL)
     {
         sumvariable = sumvariable + root->key;
-        //cout <<"added"<<sumvariable <<"kazhinju";
-        sumofnodes(root->left);
-        sumofnodes(root->right);
+
+        sumOfKeys(root->left);
+        sumOfKeys(root->right);
     }
     return sumvariable;
 }
-int sumwrap(Node *root)
+
+int sumOfKeysCaller(Node *root)
 {
     sumvariable = 0;
-    return sumofnodes(root);
+    return sumOfKeys(root);
 }
-int numbervariable = 0;
-int numberofnodes(Node *root)
+
+int totalNumberOfNodes(Node *root)
 {
     if (root != NULL)
     {
         numbervariable = numbervariable + 1;
-        //cout <<"added"<<sumvariable <<"kazhinju";
-        numberofnodes(root->left);
-        numberofnodes(root->right);
+
+        totalNumberOfNodes(root->left);
+        totalNumberOfNodes(root->right);
     }
     return numbervariable;
 }
-int numberofnodeswrap(Node *root)
+
+int totalNumberOfNodesCaller(Node *root)
 {
     numbervariable = 0;
-    return numberofnodes(root);
+    return totalNumberOfNodes(root);
 }
-Node *search(Node *root, int key)
+
+Node *searchNodeForKey(Node *root, int key)
 {
     Node *current = root;
     while (current != NULL)
@@ -270,88 +358,4 @@ Node *search(Node *root, int key)
         current = current->key < key ? current->right : current->left;
     }
     return current;
-}
-int main()
-{
-    Node *root = NULL;
-
-    // root = insert(root, 9);
-    // root = insert(root, 5);
-    // root = insert(root, 10);
-    // root = insert(root, 0);
-    // root = insert(root, 6);
-    // root = insert(root, 11);
-    // root = insert(root, -1);
-    // root = insert(root, 1);
-    // root = insert(root, 2);
-
-    // cout << "Preorder traversal of the "
-    //         "constructed AVL tree is \n";
-    // preOrder(root);
-
-    // root = deleteNode(root, 10);
-    // cout << "\nPreorder traversal after"
-    //      << " deletion of 10 \n";
-    // preOrder(root);
-
-    string numberOfLines, eachLine;
-    int integerInsert, integerDelete, lastLineInteger;
-    getline(cin, numberOfLines);
-    int numberOfLinesInteger = stoi(numberOfLines);
-    for (int i = 0; i < numberOfLinesInteger; i++)
-    {
-        getline(cin, eachLine);
-        stringstream ss(eachLine);
-        string word;
-        string eachword[1000];
-        for (int j = 0; ss >> word; j++)
-        {
-            eachword[j] = word;
-        }
-
-        if ((eachword[0].compare("A")) == 0)
-        {
-            for (int iterator = 1; eachword[iterator].compare("\0") != 0; iterator++)
-            {
-                //cout << eachword[iterator];
-                integerInsert = stoi(eachword[iterator]);
-                root = insert(root, integerInsert);
-            }
-        }
-        else if ((eachword[0].compare("R")) == 0)
-        {
-            for (int iterator = 1; eachword[iterator].compare("\0") != 0; iterator++)
-            {
-                //cout << eachword[iterator];
-                integerDelete = stoi(eachword[iterator]);
-                root = deleteNode(root, integerDelete);
-            }
-        }
-        else
-        {
-            for (int iterator = 0; eachword[iterator].compare("\0") != 0; iterator++)
-            {
-                lastLineInteger = stoi(eachword[iterator]);
-                Node *thisisit = search(root, lastLineInteger);
-                if (thisisit != NULL)
-                {
-                    int heightofkey = height(thisisit);
-                    cout << heightofkey;
-                    //preOrder(thisisit);
-                    cout << " " << minValueNode(thisisit)->key;
-                    cout << " " << maxValueNode(thisisit)->key;
-                    int sum = sumwrap(thisisit);
-                    int numberofnodes = numberofnodeswrap(thisisit);
-                    double average = (sum * 1.0) / numberofnodes;
-                    float value = (int)(average * 10 + .5);
-                    cout << " " << (float)value / 10 << endl;
-                }
-                else
-                {
-                    cout << "-1" << endl;
-                }
-            }
-        }
-    }
-    return 0;
 }
